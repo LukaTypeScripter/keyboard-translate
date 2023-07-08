d931a69ab04b5901109d74e583187d0056f48206
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-07-08 16:08:47 +0400

    interfaces

--- src/components/interfaces/interface.ts
+++ src/components/interfaces/interface.ts
@@ -3,6 +3,7 @@
 export interface HeaderProps {
     isChecked: boolean;
     toggleChecked: () => void
+    setWord:React.Dispatch<React.SetStateAction<string>>
 }
 
 export interface SmallModals {
@@ -12,4 +13,48 @@ export interface SmallModals {
 
 export interface SearchProps {
     isChecked: boolean
+    setWord:React.Dispatch<React.SetStateAction<string>>
 }
+
+
+export interface Word {
+    word: string;
+    phonetic: string;
+    phonetics: Phonetic[];
+    meanings: Meaning[];
+    synonyms: string[];
+    antonyms: string[];
+    license: License;
+    sourceUrls: string[];
+  }
+  
+  interface Phonetic {
+    text: string;
+    audio: string;
+    sourceUrl: string;
+    license: {
+      name: string;
+      url: string;
+    };
+  }
+  
+  interface Definition {
+    definition: string;
+    synonyms: string[];
+    antonyms: string[];
+  }
+  
+  interface Meaning {
+    partOfSpeech: string;
+    definitions: Definition[]; // Include definitions property here
+    synonyms: string[];
+    antonyms: string[];
+  }
+  
+  interface License {
+    name: string;
+    url: string;
+  }
+  export interface MainProps {
+    wordData:Word
+  }
