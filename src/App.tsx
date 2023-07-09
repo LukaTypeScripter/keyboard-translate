bedbf8e5932e98396e53164a9668cb828ba90aa1
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-07-09 14:13:22 +0400

    added localstorage for saving data and darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,98 @@
-import { useEffect, useState } from 'react'
-import Header from './components/Header'
-import Main from './components/Main'
-import axios from 'axios';
-import { Word } from './components/interfaces/interface';
-//
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import axios from "axios";
+import { Word } from "./components/interfaces/interface";
+import Loader from "./components/Loader";
+
 function App() {
-  const [isChecked, setIsChecked] = useState(false)
-  const [word,setWord] = useState('keyboard')
-  const [wordData,setWordData] = useState<Word[]>([])
-  const [loading,setloading] = useState(false)
-  const [activeItem, setActiveItem] = useState('Sans Serif');
+  const [isChecked, setIsChecked] = useState(false);
+  const [word, setWord] = useState("keyboard");
+  const [wordData, setWordData] = useState<Word[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [activeItem, setActiveItem] = useState("Sans Serif");
+  const [error, setError] = useState(false);
+
   const toggleChecked = () => {
-    setIsChecked(!isChecked)
-  }
-  let dark = isChecked ? 'dark:bg-blackvariant1 ' : '';
-  let color = isChecked ? 'dark:text-white' : '';
-  
+    setIsChecked((prevChecked) => {
+      const newChecked = !prevChecked;
+      localStorage.setItem("isChecked", JSON.stringify(newChecked));
+      return newChecked;
+    });
+  };
+
+  let dark = isChecked ? "dark:bg-blackvariant1 " : "";
+  let color = isChecked ? "dark:text-white" : "";
 
   useEffect(() => {
-    setloading(true);
-    if (word !== '') {
+    setLoading(true);
+    if (word !== "") {
       axios
         .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
         .then((response) => {
           setWordData(response.data);
-          setloading(false);
+          setLoading(false);
+          setError(false);
+          localStorage.setItem("wordData", JSON.stringify(response.data));
         })
         .catch((error) => {
           console.error(error);
-          setloading(false);
+          setLoading(false);
+          setError(true);
+          localStorage.removeItem("wordData");
         });
     } else {
       setWordData([]);
-      setloading(false);
+      setLoading(false);
     }
   }, [word]);
-const fontSans = activeItem === "Sans Serif" && 'font-sans' 
-const fontSerif = activeItem === "Serif" && "font-serif"
-const fontMono = activeItem === "Mono" && "font-mono"
-console.log(wordData)
-  return (
-    <div className={`relative z-0 ${dark} ${color} ${fontSans} ${fontSerif} ${fontMono}`} >
-      <Header toggleChecked={toggleChecked} isChecked={isChecked} setWord={setWord} activeItem={activeItem} setActiveItem={setActiveItem} />
-      <Main wordData={wordData} />
-      {loading && (<>weeee</>)}
 
+  const fontSans = activeItem === "Sans Serif" && "font-sans";
+  const fontSerif = activeItem === "Serif" && "font-serif";
+  const fontMono = activeItem === "Mono" && "font-mono";
+
+  useEffect(() => {
+    const savedWordData = localStorage.getItem("wordData");
+    if (savedWordData) {
+      setWordData(JSON.parse(savedWordData));
+    }
+  }, []);
+
+  useEffect(() => {
+    const savedChecked = localStorage.getItem("isChecked");
+    if (savedChecked !== null) {
+      setIsChecked(JSON.parse(savedChecked));
+    }
+  }, []);
+
+  return (
+    <div
+      className={`relative z-0 ${dark} ${color} ${fontSans} ${fontSerif} ${fontMono} `}
+    >
+      <Header
+        toggleChecked={toggleChecked}
+        isChecked={isChecked}
+        setWord={setWord}
+        activeItem={activeItem}
+        setActiveItem={setActiveItem}
+      />
+      {error ? (
+        <main className=" mt-20 text-center h-screen">
+          <div className="text-heading-l">😕</div>
+          <h5 className="font-bold mt-5 tablet:mt-11">No Definitions Found</h5>
+          <p className="mt-3 tablet:mt-6">
+            Sorry pal, we couldn't find definitions for the word you were
+            looking for. You can try the search again at a later time or head
+            to the web instead.
+          </p>
+        </main>
+      ) : (
+        <Main wordData={wordData} />
+      )}
+
+      {loading && <Loader />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
